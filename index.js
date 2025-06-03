0a3609db0417fb104f526539936cdd587158df0d
Andeep Sahota <andeepsahota@macbookpro.home>
2025-06-03 18:06:17 +0100

    Initial commit: functions practice

---
+++ index.js
@@ -0,0 +1,22 @@
+function sayHello(name){
+    return `Hello, ${name}!`;
+
+}
+
+console.log(sayHello("Andeep"));
+
+
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(5,7)); // expected output: 12 
+
+
+function addition (a, b){
+    return a + b;
+};
+
+console.log(add(8, 7));
+
+
